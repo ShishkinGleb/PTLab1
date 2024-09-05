6186d962f84e300ebe5a00df3b9c7400a56a9be2
Shishkin <gl.shishkin2017@yandex.ru>
2024-09-05 13:33:02 +0300

    update

--- test/test_CalcRating.py
+++ test/test_CalcRating.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 from src.Types import DataType
 from src.CalcRating import CalcRating
 import pytest
@@ -13,35 +12,35 @@ class TestCalcRating:
     def input_data(self) -> tuple[DataType, RatingsType]:
         data: DataType = {
             "Абрамов Петр Сергеевич":
-                [
-                    ("математика", 80),
-                    ("русский язык", 76),
-                    ("программирование", 100)
-                ],
+            [
+                ("математика", 80),
+                ("русский язык", 76),
+                ("программирование", 100)
+            ],
             "Петров Игорь Владимирович":
-                [
-                    ("математика", 61),
-                    ("русский язык", 80),
-                    ("программирование", 78),
-                    ("литература", 97)
-                ]
+            [
+                ("математика", 61),
+                ("русский язык", 80),
+                ("программирование", 78),
+                ("литература", 97)
+            ]
         }
-
         rating_scores: RatingsType = {
             "Абрамов Петр Сергеевич": 85.3333,
             "Петров Игорь Владимирович": 79.0000
         }
-
         return data, rating_scores
 
     def test_init_calc_rating(self,
-     input_data: tuple[DataType, RatingsType]) -> None:
+                              input_data: tuple[DataType,
+                                                RatingsType]) -> None:
         calc_rating = CalcRating(input_data[0])
         assert input_data[0] == calc_rating.data
 
     def test_calc(self, input_data: tuple[DataType, RatingsType]) -> None:
+
         rating = CalcRating(input_data[0]).calc()
         for student in rating.keys():
             rating_score = rating[student]
             assert pytest.approx(rating_score,
-             abs=0.001) == input_data[1][student]
+                                 abs=0.001) == input_data[1][student]

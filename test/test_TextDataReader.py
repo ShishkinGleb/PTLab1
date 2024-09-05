4fd07f945535dd5e85a0bddbf970e8ad471e5314
Shishkin <gl.shishkin2017@yandex.ru>
2024-09-05 13:15:07 +0300

    update

--- test/test_TextDataReader.py
+++ test/test_TextDataReader.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 import pytest
 from src.Types import DataType
 from src.TextDataReader import TextDataReader
@@ -8,23 +7,25 @@ from src.TextDataReader import TextDataReader
 class TestTextDataReader:
 
     @pytest.fixture()
-
     def file_and_data_content(self) -> tuple[str, DataType]:
         text = "Иванов Константин Дмитриевич\n" + \
-                " математика:91\n" + " химия:100\n" + \
-                "Петров Петр Семенович\n" + \
-                " русский язык:87\n" + " литература:78\n"
-        data = {"Иванов Константин Дмитриевич": [("математика", 91),
-        ("химия", 100)],
-            "Петров Петр Семенович": [("русский язык", 87),
-            ("литература", 78)]
+               " математика:91\n" + " химия:100\n" + \
+               "Петров Петр Семенович\n" + \
+               " русский язык:87\n" + " литература:78\n"
+
+        data = {
+            "Иванов Константин Дмитриевич": [
+                ("математика", 91), ("химия", 100)
+            ],
+            "Петров Петр Семенович": [
+                ("русский язык", 87), ("литература", 78)
+            ]
         }
         return text, data
 
     @pytest.fixture()
-
-    def filepath_and_data(self, file_and_data_content: tuple[str,
-                    DataType], tmpdir) -> tuple[str, DataType]:
+    def filepath_and_data(self, file_and_data_content: tuple[str, DataType],
+                          tmpdir) -> tuple[str, DataType]:
         p = tmpdir.mkdir("datadir").join("my_data.txt")
         p.write_text(file_and_data_content[0], encoding='utf-8')
         return str(p), file_and_data_content[1]

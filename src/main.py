1a49c10ba9768782c041b9038dfe33861fb27b7c
Shishkin <gl.shishkin2017@yandex.ru>
2024-09-08 13:26:11 +0300

    Final commit

--- src/main.py
+++ src/main.py
@@ -1,28 +1,29 @@
 # -*- coding: utf-8 -*-
-
 import argparse
 import sys
 from CalcRating import CalcRating
 from TextDataReader import TextDataReader
+from XMLDataReader import XMLDataReader
+from GoodStudents import GoodStudents
 
 
 def get_path_from_arguments(args) -> str:
     parser = argparse.ArgumentParser(description="Path to datafile")
-    parser.add_argument("-p", dest="path", type=str,
-        required=True, help="Path to datafile")
+    parser.add_argument("-p", dest="path", type=str, required=True,
+                        help="Path to datafile")
     args = parser.parse_args(args)
     return args.path
 
 
 def main():
     path = get_path_from_arguments(sys.argv[1:])
-
-    reader = TextDataReader()
+    reader = XMLDataReader()
     students = reader.read(path)
     print("Students: ", students)
-
     rating = CalcRating(students).calc()
     print("Rating: ", rating)
+    good_student_count = GoodStudents(students).calc()
+    print("Good students count: ", good_student_count)
 
 
 if __name__ == "__main__":

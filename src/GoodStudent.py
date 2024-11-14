1df9ebfe9adc652f0c124ec4e3db3b60966685de
Shishkin <gl.shishkin2017@yandex.ru>
2024-11-14 13:46:03 +0300

    Fix logic programm

--- src/GoodStudent.py
+++ src/GoodStudent.py
@@ -8,12 +8,12 @@ class GoodStudent:
         self.count: int = 0
 
     def calc(self) -> DataType:
+        self.count = len(self.data)
         for key in self.data:
-            sum = 0
-            num_les = 0
+            bad_mark = 0
             for subject in self.data[key]:
-                sum = subject[1] + sum
-                num_les = num_les + 1
-            if ((sum/num_les) >= 76):
-                self.count = self.count + 1
+                if (subject[1] < 76):
+                    bad_mark = bad_mark + 1
+            if (bad_mark > 0):
+                count = count - 1
         return self.count

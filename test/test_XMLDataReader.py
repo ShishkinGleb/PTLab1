1b93c3eb58b644c740bdd241dd61ef00d4afbd12
Shishkin <gl.shishkin2017@yandex.ru>
2024-11-14 09:15:21 +0300

    fix3

--- test/test_XMLDataReader.py
+++ test/test_XMLDataReader.py
@@ -8,25 +8,17 @@ class TestXMLDataReader:
 
     @pytest.fixture()
     def file_and_data_content(self) -> tuple[str, DataType]:
-        xml_mess = """<?xml version="1.0" encoding="UTF-8"?>
-        <students>
-            <student name="Клюев Роман Фёдорович">
-                <subject name="математика" score="91" />
-                <subject name="химия" score="100" />
-            </student>
-            <student name="Кузнецов Руслан Тимофеевич">
-                <subject name="русский язык" score="87" />
-                <subject name="литература" score="78" />
-            </student>
-        </students>
-        """
-
+        xml_mess = "<root>\n<student>\n<name>Куликов Юрий Петрович</name>\n" + \
+            "<math>100</math>\n<programming>100</programming>\n<litera" + \
+            "ture>76</literature>\n</student>\n<student>\n<name>Кузнецов" + \
+            " Петр Петрович</name>\n<math>91</math>\n<sociology>90</" + \
+            "sociology>\n<chemistry>61</chemistry>\n</student>\n</root>\n"
         data = {
-            "Клюев Роман Фёдорович": [
-                ("математика", 91), ("химия", 100)
+            "Куликов Юрий Петрович": [
+                ("math", 100), ("programming", 100), ("literature", 76)
             ],
-            "Кузнецов Руслан Тимофеевич": [
-                ("русский язык", 87), ("литература", 78)
+            "Кузнецов Петр Петрович": [
+                ("math", 91), ("sociology", 90), ("chemistry", 61)
             ]
         }
         return xml_mess, data
